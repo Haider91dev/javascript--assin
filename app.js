5dd5836aed430bc95b3a3bc62e343a745335e8bb
Haider91dev <154570409+Haider91dev@users.noreply.github.com>
2024-01-03 08:14:02 +0530

    assinment-1

---
+++ app.js
@@ -0,0 +1,21 @@
+// alert()
+// Q1
+
+//  alert("WELCOME TO JAVASCRIPT PROGRAM");
+ // Q2
+
+
+//  alert("Enter! Please Enter a valid password")
+
+// Q3
+
+// alert("Welcome to JS land..\n Happy Coding");
+
+// Q4
+
+// alert("Welcome to js land..");
+// alert("Happy Coding!");
+
+//Q5
+console.log("Hello! I Can run js through js console");
+
